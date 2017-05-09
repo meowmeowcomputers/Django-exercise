f2105f0fc39ec9174193fb4d3b22e8f10785cd91
meowmeowcomputers <ryansimonleon@gmail.com>
2017-05-09 11:54:56 -0500

    Added fabric file fabfile.py

---
+++ fabfile.py
@@ -0,0 +1,27 @@
+from fabric.api import run, env, sudo, cd, prefix
+
+env.hosts = ['45.76.233.87']
+env.user = 'ryan'
+
+DIR = '/home/ryan/Django-exercise'
+VENV = 'source /home/ryan/.virtualenvs/dj-exercise/bin/activate && source SECRETS.ENV'
+
+def start ():
+  with cd(DIR):
+    with prefix(VENV):
+      run('pm2 start uwsgi -- --ini uwsgi.ini > start.log')
+
+def stop ():
+  run('pm2 stop all > stop.log')
+
+def deploy ():
+  with cd(DIR):
+    run('git pull')
+
+    with prefix(VENV):
+      run('pip install -r requirements.txt  > install.log')
+
+    run('pm2 restart all > restart.log')
+
+def hello ():
+  print("Hello")
